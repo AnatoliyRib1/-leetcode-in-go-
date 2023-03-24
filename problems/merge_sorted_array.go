7db2333bfbbc77570943343b9c10f7d1b0b68578
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-03-24 11:17:18 +0300

    замена сортировки на цикл

--- problems/merge_sorted_array.go
+++ problems/merge_sorted_array.go
@@ -1,13 +1,19 @@
 package problems
 
-import "sort"
-
 func merge(nums1 []int, m int, nums2 []int, n int) []int {
 	nums1 = nums1[:m]
 	nums2 = nums2[:n]
 
 	nums1 = append(nums1, nums2[:]...)
-	sort.Ints(nums1)
+	for i, _ := range nums1 {
+		for j, _ := range nums1 {
+			if nums1[i] < nums1[j] {
+				nums1[i], nums1[j] = nums1[j], nums1[i]
+			}
+		}
+
+	}
+
 	return nums1
 
 }
