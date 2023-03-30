58fbb068f0ef447639fa7da063d02c9328a34977
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2023-03-30 11:07:32 +0300

    merge sorted array

--- problems/merge_sorted_array/merge_sorted_array.go
+++ problems/merge_sorted_array/merge_sorted_array.go
@@ -1,14 +1,22 @@
 package merge_sorted_array
 
-import "sort"
-
 func merge(nums1 []int, m int, nums2 []int, n int) []int {
-	nums1 = nums1[:m]
-	nums2 = nums2[:n]
 
-	nums1 = append(nums1, nums2[:]...)
-	sort.Ints(nums1)
+	for i := m + n - 1; m-1 > 0 && n-1 > 0; i-- {
+
+		if nums1[m-1] > nums2[n-1] {
+			nums1[i] = nums1[m-1]
+			m--
+		} else {
+			nums1[i] = nums2[n-1]
+			n--
+		}
+
+	}
+	for n-1 > 0 {
+		nums1[n-1] = nums2[n-1]
+		n--
+	}
 
 	return nums1
-
 }
